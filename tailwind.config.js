ffead0e145c040a0a68fc6f24bd812faaf862731
rustiandacode <rustiandazen09@gmail.com>
2023-04-19 00:46:16 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      screens: {
+        xl: '1100px',
+      },
+    },
+  },
+  plugins: [],
+}
